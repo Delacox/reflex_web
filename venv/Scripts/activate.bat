fce5169ad1cd44e7d3c0d4890f6876f9f9dd1a5a
Delacox <sergio.suarez.prado@gmail.com>
2024-12-27 12:54:03 +0100

    1

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sergio\Desktop\Desarrollos\Python\Reflex\venv
+set VIRTUAL_ENV=C:\Users\sergio\Desktop\Desarrollos\Python\reflex_web\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
