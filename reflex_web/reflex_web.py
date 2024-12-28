172bb22e3636d0b9081e40393fc80c20c61742e3
Delacox <sergio.suarez.prado@gmail.com>
2024-12-28 14:02:26 +0100

    add dark light button icon

--- reflex_web/reflex_web.py
+++ reflex_web/reflex_web.py
@@ -1,39 +1,69 @@
-"""Welcome to Reflex! This file outlines the steps to create a basic app."""
-
 import reflex as rx
+from reflex.style import toggle_color_mode
+ 
 
-from rxconfig import config
-
-
+# Clase de estado para gestionar el tema.
 class State(rx.State):
-    """The app state."""
+    pass
 
-    ...
-
-
+# Definimos la interfaz.
 def index() -> rx.Component:
-    # Welcome Page (Index)
-    return rx.container(
-        rx.color_mode.button(position="top-right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", size="9"),
-            rx.text(
-                "Get started by editing ",
-                rx.code(f"{config.app_name}/{config.app_name}.py"),
-                size="5",
+    return rx.box(
+            
+            rx.vstack(
+                rx.button(
+                    rx.color_mode_cond(light=rx.image(src="/dark.png"), dark=rx.image(src="/light.png")),
+                    color=rx.color_mode_cond(light="white", dark="black"),
+                    background_color= rx.color("green", 11),
+                    on_click=toggle_color_mode,
+                    width="50px",
+                ),
+                align="end",
+                margin="1em",
             ),
-            rx.link(
-                rx.button("Check out our docs!"),
-                href="https://reflex.dev/docs/getting-started/introduction/",
-                is_external=True,
+            
+            rx.box(
+                rx.text(
+                        rx.text.span(
+                        "Tu ",
+                        color=rx.color("green", 11),
+                        ),
+                        "informático",
+                        color=rx.color_mode_cond(light="black", dark="white"),
+                        font_size="48px",
+                        font_weight= 600,                    
+                    ),   
+                rx.text(
+                        "Online",
+                        color=rx.color_mode_cond(light="black", dark="white"),
+                        font_size="48px",
+                        font_weight= 600,
+                        
+                    ),
+                
+                    
+                
+                
+                font_family="Poppins",
+                margin="3em",
             ),
-            spacing="5",
-            justify="center",
-            min_height="85vh",
-        ),
-        rx.logo(),
+    )  
+    
+    
+                
+        
+
+# Configura la aplicación.
+ 
+app = rx.App(
+    stylesheets=[
+        "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
+    ],
+    theme=rx.theme(
+        appearance="dark"
     )
+    
+        
+)
 
-
-app = rx.App()
 app.add_page(index)
